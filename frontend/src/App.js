6bbad920f21f32aa642103d51f90093c0a813760
Xuan-Truong <truong.xuan99419@gmail.com>
2021-10-25 12:28:46 +0700

    Done 2.3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
